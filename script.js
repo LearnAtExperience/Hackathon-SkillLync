56480c456ad6c28cb8bb96d7d6a9c5698ec055f9
SANTHOSH SIVAN A <87321978+LearnAtExperience@users.noreply.github.com>
2022-01-01 07:52:00 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Changing the visibility mode of scroll to top icon
-let scrollToTop = document.getElementById("scroll-to-top");
-window.onscroll = function show() {
-  if (window.pageYOffset > 1275) {
-    scrollToTop.style.visibility = "visible";
-  } else {
-    scrollToTop.style.visibility = "hidden";
-   }
- }
