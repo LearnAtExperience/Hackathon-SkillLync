27ed24f89048e2e9c318c9537777fd863f45eae5
SANTHOSH SIVAN A <87321978+LearnAtExperience@users.noreply.github.com>
2022-01-01 07:54:56 +0530

    JS

---
+++ js/script.js
@@ -0,0 +1,52 @@
+document.getElementById('btn-pro-1').onclick = () => {
+	document.getElementById('my-pro-det').innerHTML = 
+	"<p id='project-content'>"+
+	"Built the landing page of Netflix with the help of 7 days free bootcamp provided by ShapeAI."+" "+
+	"This is my first Web Development project. I have learnt a lot from this bootcamp and had great experience with ShapeAI."+" "+
+	"Technologies used - HTML5, CSS3, JavaScript. This landing page is build based on mobile first paradigm. It is completely responsive and Mobile friendly."+" "+
+	"Color combinations and font styles will be almost equal to the actual Netflix Website."
+	"</p>";
+}
+
+document.getElementById('btn-pro-2').onclick = () => {
+	document.getElementById('my-pro-det').innerHTML = 
+	"<p id='project-content'>"+
+	"This is an E-Commerce Website through which user can purchase cloths from their home over online."+" "+
+	"Done this project as a part of Full-Stack Web Development course in Skill-Lync."+" "+
+	"This site is built using HTML5, CSS3, Bootstrap, JavaScript, jQuery, AJAX, JSON and XML."+" "+
+	"Utilized bootstrap grid system and CSS as well for responsiveness. It is completely responsive and mobile friendly."+" "+
+	"Utilized Asynchronous JavaScript functions, JSON and XML for retrieving data from a server to display the products in the product list page and for Add to Cart, Add to Wishlist and Edit Profile functionalities."+" "+
+	"Color and UI/UX principles are perfectly followed. So, users can easily navigate through the site. Each and every thing will be processed step by step."+" "+
+	"</p>";
+}
+
+document.getElementById('btn-pro-3').onclick = () => {
+	document.getElementById('my-pro-det').innerHTML = 
+	"<p id='project-content'>"+
+	"This is a Travel and Tourism Website through which users can make a trip for International tour."+" "+
+	"This is a Single Page Website. Done this project as Beginner Level task as a part of Virtual Internship Program (VIP) in LetsGrowMore."+" "+
+	"Technologies used - HTML5, CSS3, Bootstrap and jQuery."+" "+
+	"</p>"
+}
+
+document.getElementById('btn-pro-4').onclick = () => {
+	document.getElementById('my-pro-det').innerHTML = 
+	"<p id='project-content'>"+
+	"This is Web Application which retrieves user data with the help of API call. It is built using React App."+" "+
+	"Done this project as Intermediate Level task as a part of Virtual Internship Program (VIP)."+" "+
+	"Technologies used - React.js and CSS3. This site contains a navigation bar with a brand name and 'Get Users' button."+" "+
+	"Data will be retrieved as JSON data and displayed in a grid layout when user clicks the button."+" "+
+	"Utilized 'useState', Asynchronous JavaScript function and fetch method for this functionality."+" "+
+	"</p>"
+}
+
+document.getElementById('btn-pro-5').onclick = () => {
+	document.getElementById('my-pro-det').innerHTML = 
+	"<p id='project-content'>"+
+	"This is a Student Result Management System from which students can retrieve their examination marks by simply entering their Register Number and Date of Birth and can save it as a PDF."+" "+
+	"This site is CRUD application at admin's end. Admin can login with Department ID and Password and Create, Read, Update and Delete any of the results."+" "+
+	"Done this project as Advance Level task as a part of Virtual Internship Program (VIP) in LetsGrowMore."+" "+
+	"Technologies used - HTML5, CSS3, Bootstrap for front-end and PHP, MySQL for Backend."+" "+
+	"Once students entered their Register Number and Date of Birth, they can see their result on the screen and they can save their result in a PDF format."+" "+
+	"</p>"
+}
